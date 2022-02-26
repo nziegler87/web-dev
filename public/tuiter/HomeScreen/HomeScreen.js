983cfb332891020a69fdd038feb5179bc5bf71e8
nziegler <ziegler.n@husky.neu.edu>
2022-02-26 07:07:45 -0500

    parameterized side bar seems to be working

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -6,7 +6,7 @@ import PostList from "../PostList";
     $('#wd-home').append(`
         <div class="row mt-2">
             <div class="col-2 col-lg-1 col-xl-2">
-                ${NavigationSidebar()}
+                ${NavigationSidebar("home")}
             </div>   
             <div class="col-10 col-lg-7 col-xl-6">
                 ${PostList()}
--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -4,35 +4,35 @@ const NavigationSidebar = (active) => {
             <a class="list-group-item text-center text-xl-start" href="/">
                 <i class="fa-brands fa-twitter"></i>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="../HomeScreen/home.html">
+            <a class="list-group-item text-center text-xl-start ${active === "home" ? "active" : ""}" href="../HomeScreen/home.html">
                 <i class="fa fa-house"></i>
                 <span class="d-none d-xl-inline-flex">Home</span>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="../ExploreScreen/explore.html">
+            <a class="list-group-item text-center text-xl-start ${active === "explore" ? "active" : ""}" href="../ExploreScreen/explore.html">
                 <i class="fa fa-hashtag"></i>
                 <span class="d-none d-xl-inline-flex">Explore</span>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="/">
+            <a class="list-group-item text-center text-xl-start ${active === "notifications" ? "active" : ""}" href="/">
                 <i class="fa fa-bell"></i>
                 <span class="d-none d-xl-inline-flex">Notifications</span>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="/">
+            <a class="list-group-item text-center text-xl-start ${active === "messages" ? "active" : ""}" href="/">
                 <i class="fa fa-envelope"></i>
                 <span class="d-none d-xl-inline-flex">Messages</span>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="/">
+            <a class="list-group-item text-center text-xl-start ${active === "bookmarks" ? "active" : ""}" href="/">
                 <i class="fa fa-bookmark"></i>
                 <span class="d-none d-xl-inline-flex">Bookmarks</span>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="/">
+            <a class="list-group-item text-center text-xl-start ${active === "lists" ? "active" : ""}" href="/">
                 <i class="fa fa-list"></i>
                 <span class="d-none d-xl-inline-flex">Lists</span>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="/">
+            <a class="list-group-item text-center text-xl-start ${active === "profile" ? "active" : ""}" href="/">
                 <i class="fa fa-user"></i>
                 <span class="d-none d-xl-inline-flex">Profile</span>
             </a>
-            <a class="list-group-item text-center text-xl-start" href="/">
+            <a class="list-group-item text-center text-xl-start ${active === "more" ? "active" : ""}" href="/">
                 <span class="fa-stack wd-stack-small">
                     <i class="fa fa-circle fa-stack-2x"></i>
                     <i class="fa fa-ellipsis fa-stack-1x text-dark"></i>
